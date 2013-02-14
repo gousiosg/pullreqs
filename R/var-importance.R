8e96e970ea669a78c81e5d21b4d6ddbcba1162e7
Georgios Gousios <gousiosg@gmail.com>
2013-02-14 16:29:16 +0100

    Re-order experiment run, reverse order of labels in chart

--- R/var-importance.R
+++ R/var-importance.R
@@ -3,11 +3,14 @@
 
 rm(list = ls(all = TRUE))
 
+source(file = "R/packages.R")
 source(file = "R/variables.R")
 source(file = "R/utils.R")
 source(file = "R/classification.R")
 source(file = "R/merge-time.R")
 source(file = "R/merge-decision.R")
+
+library(ggplot)
 
 run.rf.varimp <- function(classifname, model, sampler, data, smpl_size, runs) {
   varimp <- rf.varimp(model, sampler, data, smpl_size, runs)
@@ -17,7 +20,7 @@ run.rf.varimp <- function(classifname, model, sampler, data, smpl_size, runs) {
 
   p <- ggplot(varimp, aes(y = reorder(MeanDecreaseAccuracy, var), x = MeanDecreaseAccuracy)) +
     geom_point(size = 3) +
-    scale_y_discrete("Variable", label = varimp$var) +
+    scale_y_discrete("Variable", label = rev(varimp$var)) +
     xlab("Mean Decrease in Accuracy") +
     ggtitle(chart.title)
 
@@ -30,15 +33,17 @@ dfs <- load.all(dir=data.file.location, pattern="*.csv$")
 dfs <- addcol.merged(dfs)
 all <- merge.dataframes(dfs)
 
-# run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 1000, 50)
-# run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 1000, 100)
-# run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 10000, 50)
-# run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 10000, 100)
-# run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 40000, 50)
-#
-#
-# run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 1000, 50)
-# run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 1000, 100)
-# run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 10000, 50)
-# run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 10000, 100)
-# run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 4000, 50)
+run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 1000, 50)
+run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 1000, 50)
+
+run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 1000, 100)
+run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 1000, 100)
+
+run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 10000, 50)
+run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 10000, 50)
+
+run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 10000, 100)
+run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 10000, 100)
+
+run.rf.varimp("Merge decision", merge.decision.model, prepare.data.mergedecision, all, 40000, 50)
+run.rf.varimp("Merge time", merge.time.model, prepare.data.mergetime, all, 4000, 50)
